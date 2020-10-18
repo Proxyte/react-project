478a030dcf8490e7c66b73b97ddd6e6689c708ce
Nurbol <n.jalmukhambetov@petrel.ai>
2020-10-18 10:44:20 +0600

    Updates

---
+++ precache-manifest.9c9b21db990e78c6526b3ead611435dc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6acfcba4a150b5e30ef8669a07edf8b5",
+    "url": "/react-project/index.html"
+  },
+  {
+    "revision": "b187ebb467e3f7db87ba",
+    "url": "/react-project/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7d94196fb45de7b66572",
+    "url": "/react-project/static/js/2.3f1142e5.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/react-project/static/js/2.3f1142e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b187ebb467e3f7db87ba",
+    "url": "/react-project/static/js/main.df98fd7d.chunk.js"
+  },
+  {
+    "revision": "ef0ba607d25339a84d10",
+    "url": "/react-project/static/js/runtime-main.ec1ffa47.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-project/static/media/logo.5d5d9eef.svg"
+  }
+]);
