0034601d627e923785592b82d952013dfb78cbd4
Nurbol <n.jalmukhambetov@petrel.ai>
2020-10-18 10:50:29 +0600

    fix(*): added some text

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
     <div className="App">
       <h1>My react application in Github</h1>
       <h2>Author: Jalmukhambetov Nurbol</h2>
+      <span>Nice to know about Github Pages</span>
     </div>
   );
 }
