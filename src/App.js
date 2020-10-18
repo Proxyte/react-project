bf170adb13a1bee4c971f7970db05129d19e4760
Nurbol <n.jalmukhambetov@petrel.ai>
2020-10-18 10:47:17 +0600

    fix(*): removed error from title

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>My react application in Guthub</h1>
+      <h1>My react application in Github</h1>
       <h2>Author: Jalmukhambetov Nurbol</h2>
     </div>
   );
